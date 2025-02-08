bca7b21be2139dc65050ef683bc515589cdbf10c
sbu77blr <sbumsrit61blr@gmail.com>
2025-02-09 00:46:58 +0530

    Updated Soln Template : Added 3 functions.
    Commonly used functions for INPUT, DISPLAY and CLEARing of the LL has been added.

--- LinkedList_Soln_Template.cpp
+++ LinkedList_Soln_Template.cpp
@@ -12,8 +12,68 @@ class Node {
         // Other methods as required.
 };
 
+// Commonly needed LL Functions.
+int inputLL(Node* head);
+void displayLL(Node* head);
+void clearLL(Node* head);
+
 int main(void) {
     // Core Code for the operation.
 
     return 0;
+}
+
+
+int inputLL(Node* head) 
+{
+    int len = 0;
+    cout << "Enter elements of LL (-1 to stop entering..) : ";
+    int ele;
+    cin >> ele;
+    if(ele == -1)   return 0;
+    head->data = ele;
+    len++;
+    
+    Node* ptr = head;
+
+    cin >> ele;
+    while(ele != -1) {
+        Node* newNode = new Node(ele);
+        ptr->next = newNode;
+        ptr = newNode;
+        len++;
+        cin >> ele;
+    }
+    cout << "Inputted " << len << " nodes!" << endl;
+    return len;
+}
+
+void displayLL(Node* head) {
+    if(head == nullptr)
+    {
+        cout << "EMPTY List!\n";
+        return;
+    }
+    Node* ptr = head;
+
+    while(ptr != nullptr) {
+        cout << ptr->data << " -> ";
+        ptr = ptr->next;
+    }
+    cout << "|\n";
+}
+
+void clearLL(Node* head) {
+    if(head == nullptr)
+        return;
+
+    Node* ptr = head;
+    Node* temp = nullptr;
+
+    while(ptr != nullptr) {
+        temp = ptr->next;
+        free(ptr);
+        ptr = temp;
+    }
+    return;
 }
