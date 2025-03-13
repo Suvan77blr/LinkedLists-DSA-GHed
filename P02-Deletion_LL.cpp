d8197c93b822c0a10e11e295ed474cad504318af
Suvan77blr <sbumsrit61blr@gmail.com>
2025-03-13 17:13:40 +0530

    LL P02 : Deletion_Prog - 75% DONE
    Error coming during the updation of the head object.
    Basic Ops done ... Object-based integration w/ menu approach pending.

--- P02-Deletion_LL.cpp
+++ P02-Deletion_LL.cpp
@@ -10,11 +10,14 @@ class Node {
         Node(int data = 0, Node* next = nullptr) : data(data), next(next) {}
 
         // Other methods as required.
+        Node* deleteHead(Node* head);
+        Node* deleteTail(Node* head);
+        Node* deleteBasedOnIndex(Node* head, int index);
+        Node* deleteBasedOnValue(Node* head, int val, bool * isDeleted);
 };
 
 // CORE Functions.
-// Node* deleteHead(Node* head) {
-Node* deleteHead(Node* head) 
+Node* Node :: deleteHead(Node* head) 
 {
     if(head == nullptr) 
         return nullptr;
@@ -24,18 +27,17 @@ Node* deleteHead(Node* head)
     return newHead;
 }
 
-void deleteTail(Node** head) 
+Node* Node :: deleteTail(Node* head) 
 {
-    if(!*head) return;
+    if(!head) return head;
 
-    if((*head)->next == nullptr)
+    if(head->next == nullptr)
     {
-        delete (*head);
-        (*head) = nullptr;
-        return;
+        delete head;
+        return nullptr;
     }
     
-    Node* ptr = (*head);
+    Node* ptr = head;
 
     // Traverse to the node, just before the Tail.
     while(ptr->next->next != nullptr) {
@@ -45,13 +47,70 @@ void deleteTail(Node** head)
     Node* tailNode = ptr->next;
     delete tailNode;
     ptr->next = nullptr;
+    return head;
+}
+
+Node* Node :: deleteBasedOnIndex(Node* head, int index) {
+    if(head == nullptr)
+        return nullptr;
+    
+    if(index == 0)
+    {
+        Node* temp = head->next;
+        delete head;
+        return temp;
+    }
+
+    Node* ptr = head;
+    int i=0;
+    while(i < index-1) {
+        ptr = ptr->next;
+        if(ptr->next == nullptr)
+        {
+            cout << "Index out of bounds." << endl;
+            return head;
+        }
+        i++;
+    }
+
+    Node* temp = ptr->next;
+    ptr->next = ptr->next->next;
+    delete temp;
+    return head;
+}
+
+Node* Node :: deleteBasedOnValue(Node* head, int val, bool * isDeleted)
+{
+    if(head == nullptr)
+        return head;
+
+    
+    if(head->data == val)
+    {
+        Node* temp = head->next;
+        delete head;
+        *isDeleted = true;
+        return temp;
+    }
+
+    Node* ptr = head;
+    while(ptr->next != NULL) {
+        if(ptr->next->data == val) {
+            Node* temp = ptr->next;
+            ptr->next = ptr->next->next;
+            delete temp;
+            *isDeleted = true;
+            return head;
+        }
+    }
+    return head;
 }
 
 // Commonly needed LL Functions.
 int inputLL(Node** head);
 void displayLL(Node* head);
 void clearLL(Node* head);
-
+int menu();
 
 int main(void) {
     Node* head = new Node();
@@ -62,12 +121,49 @@ int main(void) {
     displayLL(head);
 
     // Core Code for the operation.
-    // head = deleteHead(head);
-    // cout << "\nLL ( after deleteHead() ): ";
-    // displayLL(head);
-
-    // head = deleteTail(head);
-    deleteTail(&head);
+    int ch;
+    bool bExit = false;
+    while(!bExit) {
+        ch = menu();
+        switch(ch){
+            case 1:
+                cout << "Delete-HEAD initiated..\n";
+                head = head->deleteHead(head);     
+            break;
+            case 2:
+                cout << "Delete-TAIL initiated..\n";
+                head = head->deleteTail(head);     
+            break;
+            case 3:
+                cout << "Delete-INDEX initiated..\n";
+            
+                int index;
+                cout << "Enter the index to delete : ";
+                cin >> index;
+                head = head->deleteBasedOnIndex(head, index);
+            break;
+            case 4:
+                cout << "Delete-VALUE initiated..\n";
+                bool isDeleted;
+                int val; 
+                cout << "Enter the value to delete : ";
+                head = head->deleteBasedOnValue(head, val, &isDeleted);
+                if(isDeleted){
+                    cout << "Node deleted.\n";
+                }     
+                else {cout << "Node-VALUE not found.\n";}
+            break;
+            case 0:
+                clearLL(head);
+                cout << "Exiting the interface ... ";
+                bExit = true;
+            break;
+            
+            default: 
+                cout << "Invalid choice .. try again" << endl;
+        }
+    }
+    
     cout << "\nLL ( after deleteTail() ): ";
     displayLL(head);
 
@@ -75,7 +171,6 @@ int main(void) {
     cout << "\nCleared all the nodes.\n";
     return 0;
 }
-
 
 int inputLL(Node** head) 
 {
@@ -132,4 +227,15 @@ void clearLL(Node* head) {
         ptr = temp;
     }
     return;
+}
+
+int menu() {
+    cout << "\nEnter accordingly : " << endl;
+    cout << "\t\'1\' - Delete-Head. \t\t\t  \'2\' - Delete-Tail." << endl;
+    cout << "\t\'3\' - Delete-Based-On-INDEX (0 - based)";
+    cout << "\t  \'4\' - Delete-Based-On-VALUE " << endl;
+    cout << "\t\'0\' - EXIT" << endl;;
+    cout << "Your choice : ";
+    int ch; cin >> ch;
+    return ch;
 }
